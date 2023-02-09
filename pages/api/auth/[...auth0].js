5bdd2fb0945dddb03c30769d808b1d3ab67758d5
--unset <meetsuthar64@gmail.com>
2023-02-09 01:49:22 -0500

    Running Project

--- pages/api/auth/[...auth0].js
+++ pages/api/auth/[...auth0].js
@@ -4,12 +4,12 @@ import { handleAuth, handleLogin, handleLogout } from '@auth0/nextjs-auth0';
 export default handleAuth({
     async login(req, res) {
         await handleLogin(req, res, {
-            returnTo: "https://algoshare.vercel.app//problems",
+            returnTo: "https://algoshare.vercel.app/problems",
         });
     },
     async logout(req, res) {
         await handleLogout(req, res, {
-            returnTo: "https://algoshare.vercel.app//problems"
+            returnTo: "https://algoshare.vercel.app/problems"
         })
     }
 });
